79f080d37289f1d5e845147dce59add36d69605b
Robert Lemke <robert@typo3.org>
2011-07-18 19:01:59 +0200

    Add support for external deployment files (must be located in Build/Deploy/)

--- Classes/Domain/Service/DeploymentService.php
+++ Classes/Domain/Service/DeploymentService.php
@@ -6,6 +6,8 @@ namespace TYPO3\Deploy\Domain\Service;
  *                                                                        *
  *                                                                        */
 
+use \TYPO3\Deploy\Domain\Model\Deployment;
+
 /**
  * A deployment service
  *
@@ -19,25 +21,13 @@ class DeploymentService {
 	 * @return \TYPO3\Deploy\Domain\Model\Deployment
 	 */
 	public function getDeployment($deploymentName) {
-		$deployment = new \TYPO3\Deploy\Domain\Model\Deployment('Integration');
+		$deploymentPathAndFilename = FLOW3_PATH_ROOT . 'Build/Deploy/' . $deploymentName . '.php';
+		if (!file_exists($deploymentPathAndFilename)) {
+			exit(sprintf ("The deployment file %s does not exist.\n", $deploymentPathAndFilename));
+		}
 
-		$application = new \TYPO3\Deploy\Applications\FLOW3();
-		$application->setOption('repositoryUrl', 'ssh://review.networkteam.com:29418/flow3/projects/rossmann/distributions/MyRossmann.git');
-		$application->setOption('deploymentPath', '/home/flow3-integration/sites/myrossmann-deploy');
-		$deployment->addApplication($application);
-
-		$workflow = new \TYPO3\Deploy\Domain\Model\SimpleWorkflow();
-		$deployment->setWorkflow($workflow);
-
-		$node = new \TYPO3\Deploy\Domain\Model\Node('builder');
-		$node->setHostname('myrossmann-integration.dev.networkteam.com');
-		$node->setOption('username', 'flow3-integration');
-
-		// TODO Make application options overridable per node
-
-		$application->addNode($node);
-
-		$deployment->addNode($node);
+		$deployment = new Deployment($deploymentName);
+		require($deploymentPathAndFilename);
 		return $deployment;
 	}
 
