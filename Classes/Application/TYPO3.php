3a74dc1a9a503907edb24d5760476d5ff45c8534
Robert Lemke <robert@typo3.org>
2011-08-03 16:37:58 +0200

    [FEATURE] Added a "TYPO3" application
    The TYPO3 application also provides a "import site" task.

---
+++ Classes/Application/TYPO3.php
@@ -0,0 +1,54 @@
+<?php
+namespace TYPO3\Deploy\Application;
+
+/*                                                                        *
+ * This script belongs to the FLOW3 package "TYPO3.Deploy".               *
+ *                                                                        *
+ *                                                                        */
+
+use \TYPO3\Deploy\Domain\Model\Workflow;
+use \TYPO3\Deploy\Domain\Model\Deployment;
+
+/**
+ * A TYPO3 application template
+ *
+ * @license http://www.gnu.org/licenses/gpl.html GNU General Public License, version 3 or later
+ */
+class TYPO3 extends \TYPO3\Deploy\Domain\Model\Application {
+
+	/**
+	 * Constructor
+	 */
+	public function __construct() {
+		parent::__construct('TYPO3');
+	}
+
+	/**
+	 * Register tasks for this application
+	 *
+	 * @param \TYPO3\Deploy\Domain\Model\Workflow $workflow
+	 * @param \TYPO3\Deploy\Domain\Model\Deployment $deployment
+	 * @return void
+	 */
+	public function registerTasks(Workflow $workflow, Deployment $deployment) {
+		parent::registerTasks($workflow, $deployment);
+
+		$workflow
+			->forApplication($this, 'initialize', array(
+				'typo3.deploy:flow3:createdirectories'
+			))
+			->afterTask('typo3.deploy:gitcheckout', array(
+				'typo3.deploy:flow3:symlink'
+			), $this)
+			->afterTask('typo3.deploy:gitcheckout', array(
+				'typo3.deploy:flow3:setfilepermissions'
+			), $this)
+			->forApplication($this, 'migrate', array(
+				'typo3.deploy:flow3:migrate',
+				'typo3.deploy:typo3:importsite',
+			)
+		);
+	}
+
+}
+?>
---
+++ Classes/Task/TYPO3/ImportSiteTask.php
@@ -0,0 +1,58 @@
+<?php
+namespace TYPO3\Deploy\Task\TYPO3;
+
+/*                                                                        *
+ * This script belongs to the FLOW3 package "TYPO3.Deploy".               *
+ *                                                                        *
+ *                                                                        */
+
+use \TYPO3\Deploy\Domain\Model\Node;
+use \TYPO3\Deploy\Domain\Model\Application;
+use \TYPO3\Deploy\Domain\Model\Deployment;
+
+/**
+ * Task for importing content into TYPO3
+ *
+ * @license http://www.gnu.org/licenses/gpl.html GNU General Public License, version 3 or later
+ */
+class ImportSiteTask extends \TYPO3\Deploy\Domain\Model\Task {
+
+	/**
+	 * @inject
+	 * @var \TYPO3\Deploy\Domain\Service\ShellCommandService
+	 */
+	protected $shell;
+
+	/**
+	 * Execute this task
+	 *
+	 * @param \TYPO3\Deploy\Domain\Model\Node $node
+	 * @param \TYPO3\Deploy\Domain\Model\Application $application
+	 * @param \TYPO3\Deploy\Domain\Model\Deployment $deployment
+	 * @param array $options
+	 * @return void
+	 */
+	public function execute(Node $node, Application $application, Deployment $deployment, array $options = array()) {
+		if (!$application->hasOption('sitePackageKey')) {
+			throw new \Exception('Missing site package key.', 1312312646);
+		}
+		$targetPath = $deployment->getApplicationReleasePath($application);
+		$sitePackageKey = $application->getOption('sitePackageKey');
+		$this->shell->execute('cd ' . $targetPath . ' && FLOW3_CONTEXT=Production ./flow3 typo3.typo3:site:import < Packages/Sites/' . $sitePackageKey . '/Resources/Private/Content/Sites.xml', $node, $deployment);
+	}
+
+	/**
+	 * Rollback the task
+	 *
+	 * @param \TYPO3\Deploy\Domain\Model\Node $node
+	 * @param \TYPO3\Deploy\Domain\Model\Application $application
+	 * @param \TYPO3\Deploy\Domain\Model\Deployment $deployment
+	 * @param array $options
+	 * @return void
+	 */
+	public function rollback(Node $node, Application $application, Deployment $deployment, array $options = array()) {
+		// TODO Implement rollback
+	}
+
+}
+?>
