80469f288306dbc6f2b2356ecfaee88d40f1f61b
Christopher Hlubek <hlubek@networkteam.com>
2011-10-25 13:38:15 +0200

    [FEATURE] A HTTP test task for smoke-tests
    This change adds a HTTP test task based on the Caretaker HTTP test
    for testing HTTP requests from the local machine (using PHP curl) or
    remotely (using curl CLI over SSH).
    
    This can certainly be improved but is a first base for smoke tests.
    
    Change-Id: I34e798677e4e5252f5e1c161dc513b36626ee216

---
+++ Classes/Task/Test/HttpTestTask.php
@@ -0,0 +1,312 @@
+<?php
+namespace TYPO3\Surf\Task\Test;
+
+/*                                                                        *
+ * This script belongs to the FLOW3 package "TYPO3.Surf".                 *
+ *                                                                        *
+ *                                                                        */
+
+use TYPO3\Surf\Domain\Model\Node;
+use TYPO3\Surf\Domain\Model\Application;
+use TYPO3\Surf\Domain\Model\Deployment;
+
+use TYPO3\FLOW3\Annotations as FLOW3;
+
+/**
+ * A task for testing HTTP request
+ *
+ * This task could be used to do smoke-tests against web applications in release (e.g. on a virtual host mounted
+ * on the "next" symlink).
+ */
+class HttpTestTask extends \TYPO3\Surf\Domain\Model\Task {
+
+	/**
+	 * @FLOW3\Inject
+	 * @var \TYPO3\Surf\Domain\Service\ShellCommandService
+	 */
+	protected $shell;
+
+	/**
+	 * Execute this task
+	 *
+	 * @param \TYPO3\Surf\Domain\Model\Node $node
+	 * @param \TYPO3\Surf\Domain\Model\Application $application
+	 * @param \TYPO3\Surf\Domain\Model\Deployment $deployment
+	 * @param array $options
+	 * @return void
+	 */
+	public function execute(Node $node, Application $application, Deployment $deployment, array $options = array()) {
+		if (!isset($options['url'])) {
+			throw new \Exception('No url option provided for HttpTestTask', 1319534939);
+		}
+
+		// $this->logRequest($node, $application, $deployment, $options);
+
+		if (isset($options['remote']) && $options['remote'] === TRUE) {
+			$result = $this->executeRemoteCurlRequest($options['url'], $node, $deployment);
+		} else {
+			$deployment->getLogger()->log('Requesting URL "' . $options['url'] . '"', LOG_DEBUG);
+			$result = $this->executeLocalCurlRequest(
+				$options['url'],
+				isset($options['timeout']) ? $options['timeout'] : NULL,
+				isset($options['port']) ? $options['port'] : NULL,
+				isset($options['method']) ? $options['method'] : NULL,
+				isset($options['username']) ? $options['username'] : NULL,
+				isset($options['password']) ? $options['password'] : NULL,
+				isset($options['data']) ? $options['data'] : NULL,
+				isset($options['proxy']) ? $options['proxy'] : NULL,
+				isset($options['proxyPort']) ? $options['proxyPort'] : NULL
+			);
+		}
+
+		$this->assertExpectedStatus($options, $result);
+		$this->assertExpectedHeaders($options, $result);
+		$this->assertExpectedRegexp($options, $result);
+	}
+
+	/**
+	 * @param array $options
+	 * @param array $result
+	 * @return void
+	 */
+	protected function assertExpectedStatus(array $options, array $result) {
+		if (!isset($options['expectedStatus'])) return;
+
+		if ((int)$result['info']['http_code'] !== (int)$options['expectedStatus']) {
+			throw new \Exception('Expected status code ' . $options['expectedStatus'] . ' but got ' . $result['info']['http_code'], 1319536619);
+		}
+	}
+
+	/**
+	 * @param array $options
+	 * @param array $result
+	 * @return void
+	 */
+	protected function assertExpectedHeaders(array $options, array $result) {
+		if (!isset($options['expectedHeaders'])) return;
+
+		$expectedHeaders = array();
+		$expectedHeadersConfiguration = $options['expectedHeaders'];
+		if ($expectedHeadersConfiguration) {
+			$configurationLines = explode(chr(10), $expectedHeadersConfiguration);
+			foreach ($configurationLines as $configurationLine){
+				list($headerName, $headerValue) = explode(':', $configurationLine, 2);
+				$headerName = trim($headerName);
+				$headerValue = trim($headerValue);
+				if ($headerName && $headerValue) {
+					$expectedHeaders[$headerName] = $headerValue;
+				}
+			}
+		}
+
+		if (count($expectedHeaders) > 0) {
+			foreach ($expectedHeaders as $headerName => $expectedValue) {
+				if (!isset($result['headers'][$headerName])) {
+					throw new \Exception('Expected header "' . $headerName . '" not present', 1319535441);
+				} else {
+					$headerValue = $result['headers'][$headerName];
+					$partialSuccess = $this->testSingleHeader($headerValue, $expectedValue);
+					if (!$partialSuccess) {
+						throw new \Exception('Expected header value for "' . $headerName . '" did not match "' . $expectedValue . '": "' . $headerValue . '"', 1319535733);
+					}
+				}
+			}
+		}
+	}
+
+	/**
+	 * @param array $options
+	 * @param array $result
+	 * @return void
+	 */
+	protected function assertExpectedRegexp(array $options, array $result) {
+		if (!isset($options['expectedRegexp']))	return;
+
+		$expectedRegexp = array();
+		$expectedRegexpConfiguration = $options['expectedRegexp'];
+		if ($expectedRegexpConfiguration) {
+			$expectedRegexp = explode(chr(10), $expectedRegexpConfiguration);
+		}
+
+		if (count($expectedRegexp) > 0) {
+			foreach ($expectedRegexp as $regexp) {
+				$regexp = trim($regexp);
+				if ($regexp !== '' && !preg_match($regexp, $result['body'])) {
+					throw new \Exception('Body did not match expected regular expression "' . $regexp . '": ' . substr($result['body'], 0, 200) . (strlen($result['body']) > 200 ? '...' : ''), 1319536046);
+				}
+			}
+		}
+	}
+
+	/**
+	 * Compare returned HTTP headers with expected values
+	 *
+	 * @param string $headerValue
+	 * @param string $expectedValue
+	 * @return boolean
+	 */
+	protected function testSingleHeader($headerValue, $expectedValue) {
+		if (!$headerValue || strlen(trim($headerValue)) === 0) {
+			return FALSE;
+		}
+
+		$result = TRUE;
+
+			// = Value equals
+		if (strpos($expectedValue, '=') === 0) {
+			$result = $headerValue === trim(substr($expectedValue, 1));
+		}
+			// < Intval smaller than
+		else if (strpos($expectedValue, '<') === 0) {
+			$result = intval($headerValue) < intval(substr($expectedValue, 1));
+		}
+			// > Intval bigger than
+		else if (strpos($expectedValue, '>') === 0) {
+			$result = intval($headerValue) > intval(substr($expectedValue, 1));
+		}
+			// Default
+		else {
+			$result = $headerValue === $expectedValue;
+		}
+
+		return $result;
+	}
+
+	/**
+	 * Simulate this task
+	 *
+	 * @param Node $node
+	 * @param Application $application
+	 * @param Deployment $deployment
+	 * @param array $options
+	 * @return void
+	 */
+	public function simulate(Node $node, Application $application, Deployment $deployment, array $options = array()) {
+		// $this->logRequest($node, $application, $deployment, $options);
+	}
+
+	/**
+	 *
+	 * @param string $url Request URL
+	 * @param integer $timeout Request HTTP timeout, defaults to 0 (no timeout)
+	 * @param integer $port Request HTTP port
+	 * @param string $method Request method, defaults to GET. POST, PUT and DELETE are also supported.
+	 * @param string $username Optional username for HTTP authentication
+	 * @param string $password Optional password for HTTP authentication
+	 * @param string $data
+	 * @param string $proxy
+	 * @param integer $proxyPort
+	 * @return array time in seconds and status information im associative arrays
+	 */
+	protected function executeLocalCurlRequest($url, $timeout = NULL, $port = NULL, $method = 'GET', $username = NULL, $password = NULL, $data = '', $proxy = NULL, $proxyPort = NULL) {
+		$curl = curl_init();
+
+		curl_setopt($curl, CURLOPT_URL, $url);
+		if ($timeout !== NULL) {
+			curl_setopt($curl, CURLOPT_TIMEOUT, (int)ceil($timeout / 1000));
+		}
+
+		if ($username !== NULL && $password !== NULL) {
+			curl_setopt($curl, CURLOPT_HTTPAUTH, CURLAUTH_ANY);
+			curl_setopt($curl, CURLOPT_USERPWD, $username . ':' . $password);
+		}
+
+		if ($port !== NULL) {
+			curl_setopt($curl, CURLOPT_PORT, (int)$port);
+		}
+
+		if ($proxy !== NULL && $proxyPort !== NULL) {
+			curl_setopt($curl, CURLOPT_PROXY, $proxy);
+			curl_setopt($curl, CURLOPT_PROXYPORT, (int)$proxyPort);
+		}
+
+		switch ($method) {
+			case 'POST':
+				curl_setopt($curl, CURLOPT_CUSTOMREQUEST, 'POST');
+				curl_setopt($curl, CURLOPT_HTTPHEADER, array('Content-Length: ' . strlen($data)));
+				curl_setopt($curl, CURLOPT_POSTFIELDS, $data);
+				break;
+			case 'PUT':
+				curl_setopt($curl, CURLOPT_CUSTOMREQUEST, 'PUT');
+				curl_setopt($curl, CURLOPT_HTTPHEADER, array('Content-Length: ' . strlen($data)));
+				curl_setopt($curl, CURLOPT_POSTFIELDS, $data);
+				break;
+			case 'DELETE':
+				curl_setopt($curl, CURLOPT_CUSTOMREQUEST, 'DELETE');
+				break;
+		}
+
+		curl_setopt($curl, CURLOPT_HEADER, TRUE);
+		curl_setopt($curl, CURLOPT_RETURNTRANSFER, TRUE);
+
+		$response = curl_exec($curl);
+		$info = curl_getinfo($curl);
+		curl_close($curl);
+
+		list($headerText, $body) = preg_split('/\n[\s]*\n/', $response, 2);
+		$headers = $this->extractResponseHeaders($headerText);
+
+		return array(
+			'headers' => $headers,
+			'body' => $body,
+			'info' => $info
+		);
+	}
+
+	/**
+	 *
+	 * @param string $url Request URL
+	 * @param \TYPO3\Surf\Domain\Model\Node $node
+	 * @param \TYPO3\Surf\Domain\Model\Deployment $deployment
+	 * @return array time in seconds and status information im associative arrays
+	 */
+	protected function executeRemoteCurlRequest($url, Node $node, Deployment $deployment) {
+		$command = 'curl -s -I ' . escapeshellarg($url);
+		$head = $this->shell->execute($command, $node, $deployment, FALSE, FALSE);
+
+		$command = 'curl -s ' . escapeshellarg($url);
+		$body = $this->shell->execute($command, $node, $deployment, FALSE, FALSE);
+
+
+		list($status, $headersString) = explode(chr(10), $head, 2);
+		$statusParts = explode(' ', $status);
+		$statusCode = $statusParts[1];
+		$info = array(
+			'http_code' => $statusCode
+		);
+		$headers = $this->extractResponseHeaders(trim($headersString));
+
+		return array(
+			'headers' => $headers,
+			'body' => $body,
+			'info' => $info
+		);
+	}
+
+
+	/**
+	 * Split response into headers and body part
+	 *
+	 * @param string $headerText
+	 * @return array Extracted response headers as associative array
+	 */
+	protected function extractResponseHeaders($headerText) {
+		$headers = array();
+		if ($headerText) {
+			$headerLines = explode(chr(10), $headerText);
+			foreach ($headerLines as $headerLine) {
+				$headerParts = explode(':', $headerLine, 2);
+				if (count($headerParts) < 2) continue;
+
+				$headerName = trim($headerParts[0]);
+				$headerValue = trim($headerParts[1]);
+				if ($headerName && $headerValue) {
+					$headers[$headerName] = $headerValue;
+				}
+			}
+		}
+		return $headers;
+	}
+
+}
+?>
--- README.md
+++ README.md
@@ -96,6 +96,52 @@ This will execute the new task in the _migrate_ stage only for the application r
 
 Besides specifying the execution point via a stage, you can also give an existing task as an anchor and specify the task execution with `afterTask` or `beforeTask`.
 
+## Smoke Testing
+
+As you do automated deployments, you should check if the website is up and running
+before switching it to the live site. This is called a *Smoke Test*. We will give
+an example for using the built-in HTTP smoke test.
+
+First, you need to create a virtual host for the "<deploymentDirectory>/releases/next/Web".
+While a deployment is running, the new website will be available under this URL and can
+be used for testing.
+
+Then, add a test as follows to the deployment descriptor:
+
+```php
+   $smokeTestOptions = array(
+                'url' => 'http://your/website/which/you/want/to/test',
+                'remote' => TRUE,
+                'expectedStatus' => 200,
+                'expectedRegexp' => '/somethingYouExpectOnThePage/'
+        );
+        $workflow->defineTask('yourNamespace:smoketest', 'typo3.surf:test:httptest', $smokeTestOptions);
+
+        $workflow->addTask('yourNamespace:smoketest', 'test', $application);
+```
+
+The HTTP test has the following options:
+
+Most important options:
+
+* url (required): URL which should be loaded
+* remote: if TRUE, the smoke test is triggered through the SSH channel on the remote host
+  via command-line CURL. If false, it is triggered from the deploying host.
+* expectedStatus: expected HTTP status code
+* expectedHeaders: HTTP Header Strings which are expected (can be a multiline string, each header being on a separate line)
+* expectedRegexp: Regular Expression to test the contents of the HTTP response against
+
+Further options:
+
+* timeout (only if remote=FALSE): HTTP timeout to use
+* port (only if remote=FALSE): HTTP Port to use
+* method (only if remote=FALSE): HTTP method to use (default GET)
+* username (only if remote=FALSE): HTTP Authentication username
+* password (only if remote=FALSE): HTTP Authentication Password
+* data (only if remote=FALSE): HTTP payload
+* proxy (only if remote=FALSE): HTTP Proxy to use
+* proxyPort  (only if remote=FALSE): HTTP Proxy port to use
+
 ## COPYRIGHT
 
 The deployment package is licensed under GNU General Public License, version 3 or later (http://www.gnu.org/licenses/gpl.html). Initial development was sponsored by [networkteam - FLOW3 Agentur](http://www.networkteam.com/flow3-agentur.html).
