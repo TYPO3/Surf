4704231819165306f7de73004117ae8d05637131
Helmut Hummel <helmut.hummel@typo3.org>
2015-12-30 18:38:01 +0100

    [TASK] Fix copyright header

--- src/Command/DescribeCommand.php
+++ src/Command/DescribeCommand.php
@@ -1,18 +1,10 @@
 <?php
 namespace TYPO3\Surf\Command;
 
-/*
- * This file is part of the TYPO3 CMS project.
- *
- * It is free software; you can redistribute it and/or modify it under
- * the terms of the GNU General Public License, either version 2
- * of the License, or any later version.
- *
- * For the full copyright and license information, please read the
- * LICENSE.txt file that was distributed with this source code.
- *
- * The TYPO3 project - inspiring people to share!
- */
+/*                                                                        *
+ * This script belongs to the TYPO3 project "TYPO3 Surf".                 *
+ *                                                                        *
+ *                                                                        */
 
 use Symfony\Component\Console\Command\Command;
 use Symfony\Component\Console\Input\InputArgument;
