bc7b01dbb6d637a6f7304e840a3e91ac6fb821dd
Helmut Hummel <info@helhum.io>
2016-01-07 13:36:45 +0100

    [TASK] Add migration script for old task names

---
+++ Resources/Private/Scripts/MigrateLegacyTaskNamesToClassNames.php
@@ -0,0 +1,44 @@
+<?php
+if (PHP_SAPI !== 'cli') {
+    die('This script can only be executed from command line!');
+}
+require __DIR__ . '/../../../vendor/autoload.php';
+$final = require __DIR__ . '/../../../Migrations/Code/LegacyClassMap.php';
+
+$finder = new \Symfony\Component\Finder\Finder();
+
+$files = $finder->files()
+    ->ignoreDotFiles(true)
+    ->in(__DIR__ . '/../../../src/');
+
+$files = iterator_to_array($files->getIterator());
+$files[] = __DIR__ . '/../../../README.md';
+foreach ($files as $file) {
+    $fileContent = file_get_contents($file);
+    foreach ($final as $identifier => $className) {
+        $fileContent = str_ireplace($identifier, str_replace('\\', '\\\\', $className), $fileContent);
+    }
+    file_put_contents($file, $fileContent);
+}
+
+function getTaskIdentifier($className) {
+    return
+        substr(
+            str_replace(
+                array(
+                    'typo3\\surf',
+                    '\\task',
+                    '\\',
+                ),
+                array(
+                    'typo3.surf',
+                    '',
+                    ':',
+                ),
+                strtolower($className)
+            ),
+            0,
+            -4
+        );
+
+}
