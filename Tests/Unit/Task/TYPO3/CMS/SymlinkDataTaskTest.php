e47479b91e9bd324f1513aad93cf1d6a089b4a72
Helmut Hummel <info@helhum.io>
2016-03-30 16:11:46 +0200

    Cleanup SymlinkDataTaskTest

--- Tests/Unit/Task/TYPO3/CMS/SymlinkDataTaskTest.php
+++ Tests/Unit/Task/TYPO3/CMS/SymlinkDataTaskTest.php
@@ -14,7 +14,7 @@ use TYPO3\Surf\Task\TYPO3\CMS\SymlinkDataTask;
 use TYPO3\Surf\Tests\Unit\Task\BaseTaskTest;
 
 /**
- * Class SymlinkDataTest
+ * Class SymlinkDataTaskTest
  */
 class SymlinkDataTaskTest extends BaseTaskTest
 {
@@ -23,48 +23,19 @@ class SymlinkDataTaskTest extends BaseTaskTest
      */
     protected $task;
 
-    /**
-     * @var ShellCommandService|\PHPUnit_Framework_MockObject_MockObject
-     */
-    protected $shellMock;
-
-    /**
-     * @var Node|\PHPUnit_Framework_MockObject_MockObject
-     */
-    protected $nodeMock;
-
-    /**
-     * @var Application|\PHPUnit_Framework_MockObject_MockObject
-     */
-    protected $applicationMock;
-
-    /**
-     * @var Deployment|\PHPUnit_Framework_MockObject_MockObject
-     */
-    protected $deploymentMock;
+    public function setUp()
+    {
+        parent::setUp();
+        $this->application = new \TYPO3\Surf\Application\TYPO3\CMS('TestApplication');
+        $this->application->setDeploymentPath('/home/jdoe/app');
+    }
 
     /**
      * @return SymlinkDataTask
      */
     protected function createTask()
     {
-        $task = new SymlinkDataTask();
-        $this->shellMock = $this->getMock('TYPO3\Surf\Domain\Service\ShellCommandService');
-        $task->setShellCommandService($this->shellMock);
-        return $task;
-    }
-
-    public function setUp()
-    {
-        $this->task = $this->createTask();
-        $this->nodeMock = $this->getMockBuilder('TYPO3\Surf\Domain\Model\Node')->disableOriginalConstructor()->getMock();
-        $this->deploymentMock = $this->getMockBuilder('TYPO3\Surf\Domain\Model\Deployment')->disableOriginalConstructor()->getMock();
-
-        $this->deploymentMock->expects($this->once())
-            ->method('getApplicationReleasePath')
-            ->willReturn('/releases/current');
-
-        $this->applicationMock = $this->getMockBuilder('TYPO3\Surf\Domain\Model\Application')->disableOriginalConstructor()->getMock();
+        return new SymlinkDataTask();
     }
 
     /**
@@ -72,23 +43,15 @@ class SymlinkDataTaskTest extends BaseTaskTest
      */
     public function withoutOptionsCreatesCorrectLinks()
     {
-        $dataPath = '../../shared/Data';
-        $expectedCommands = array(
-            "cd '/releases/current'",
-            "{ [ -d {$dataPath}/fileadmin ] || mkdir -p {$dataPath}/fileadmin ; }",
-            "{ [ -d {$dataPath}/uploads ] || mkdir -p {$dataPath}/uploads ; }",
-            "ln -sf {$dataPath}/fileadmin ./fileadmin",
-            "ln -sf {$dataPath}/uploads ./uploads",
-        );
-
         $options = array();
+        $this->task->execute($this->node, $this->application, $this->deployment, $options);
 
-        $this->shellMock->expects($this->once())
-            ->method('executeOrSimulate')
-            ->with($expectedCommands, $this->nodeMock, $this->deploymentMock)
-        ;
-
-        $this->task->execute($this->nodeMock, $this->applicationMock, $this->deploymentMock, $options);
+        $dataPath = '../../shared/Data';
+        $this->assertCommandExecuted("cd '{$this->deployment->getApplicationReleasePath($this->application)}'");
+        $this->assertCommandExecuted("{ [ -d {$dataPath}/fileadmin ] || mkdir -p {$dataPath}/fileadmin ; }");
+        $this->assertCommandExecuted("{ [ -d {$dataPath}/uploads ] || mkdir -p {$dataPath}/uploads ; }");
+        $this->assertCommandExecuted("ln -sf {$dataPath}/fileadmin ./fileadmin");
+        $this->assertCommandExecuted("ln -sf {$dataPath}/uploads ./uploads");
     }
 
     /**
@@ -96,29 +59,21 @@ class SymlinkDataTaskTest extends BaseTaskTest
      */
     public function withAdditionalDirectoriesCreatesCorrectLinks()
     {
-        $dataPath = '../../shared/Data';
-        $expectedCommands = array(
-            "cd '/releases/current'",
-            "{ [ -d {$dataPath}/fileadmin ] || mkdir -p {$dataPath}/fileadmin ; }",
-            "{ [ -d {$dataPath}/uploads ] || mkdir -p {$dataPath}/uploads ; }",
-            "ln -sf {$dataPath}/fileadmin ./fileadmin",
-            "ln -sf {$dataPath}/uploads ./uploads",
-            "{ [ -d '{$dataPath}/pictures' ] || mkdir -p '{$dataPath}/pictures' ; }",
-            "ln -sf '{$dataPath}/pictures' 'pictures'",
-            "{ [ -d '{$dataPath}/test/assets' ] || mkdir -p '{$dataPath}/test/assets' ; }",
-            "ln -sf '../{$dataPath}/test/assets' 'test/assets'",
-        );
-
         $options = array(
             'directories' => array('pictures', 'test/assets'),
         );
+        $this->task->execute($this->node, $this->application, $this->deployment, $options);
 
-        $this->shellMock->expects($this->once())
-            ->method('executeOrSimulate')
-            ->with($expectedCommands, $this->nodeMock, $this->deploymentMock)
-        ;
-
-        $this->task->execute($this->nodeMock, $this->applicationMock, $this->deploymentMock, $options);
+        $dataPath = '../../shared/Data';
+        $this->assertCommandExecuted("cd '{$this->deployment->getApplicationReleasePath($this->application)}'");
+        $this->assertCommandExecuted("{ [ -d {$dataPath}/fileadmin ] || mkdir -p {$dataPath}/fileadmin ; }");
+        $this->assertCommandExecuted("{ [ -d {$dataPath}/uploads ] || mkdir -p {$dataPath}/uploads ; }");
+        $this->assertCommandExecuted("ln -sf {$dataPath}/fileadmin ./fileadmin");
+        $this->assertCommandExecuted("ln -sf {$dataPath}/uploads ./uploads");
+        $this->assertCommandExecuted("{ [ -d '{$dataPath}/pictures' ] || mkdir -p '{$dataPath}/pictures' ; }");
+        $this->assertCommandExecuted("ln -sf '{$dataPath}/pictures' 'pictures'");
+        $this->assertCommandExecuted("{ [ -d '{$dataPath}/test/assets' ] || mkdir -p '{$dataPath}/test/assets' ; }");
+        $this->assertCommandExecuted("ln -sf '../{$dataPath}/test/assets' 'test/assets'");
     }
 
     /**
@@ -126,25 +81,17 @@ class SymlinkDataTaskTest extends BaseTaskTest
      */
     public function withApplicationRootCreatesCorrectLinks()
     {
-        $dataPath = '../../../../shared/Data';
-        $expectedCommands = array(
-            "cd '/releases/current/app/dir'",
-            "{ [ -d {$dataPath}/fileadmin ] || mkdir -p {$dataPath}/fileadmin ; }",
-            "{ [ -d {$dataPath}/uploads ] || mkdir -p {$dataPath}/uploads ; }",
-            "ln -sf {$dataPath}/fileadmin ./fileadmin",
-            "ln -sf {$dataPath}/uploads ./uploads",
-        );
-
         $options = array(
             'applicationRootDirectory' => 'app/dir/'
         );
+        $this->task->execute($this->node, $this->application, $this->deployment, $options);
 
-        $this->shellMock->expects($this->once())
-            ->method('executeOrSimulate')
-            ->with($expectedCommands, $this->nodeMock, $this->deploymentMock)
-        ;
-
-        $this->task->execute($this->nodeMock, $this->applicationMock, $this->deploymentMock, $options);
+        $dataPath = '../../../../shared/Data';
+        $this->assertCommandExecuted("cd '{$this->deployment->getApplicationReleasePath($this->application)}/app/dir'");
+        $this->assertCommandExecuted("{ [ -d {$dataPath}/fileadmin ] || mkdir -p {$dataPath}/fileadmin ; }");
+        $this->assertCommandExecuted("{ [ -d {$dataPath}/uploads ] || mkdir -p {$dataPath}/uploads ; }");
+        $this->assertCommandExecuted("ln -sf {$dataPath}/fileadmin ./fileadmin");
+        $this->assertCommandExecuted("ln -sf {$dataPath}/uploads ./uploads");
     }
 
     /**
@@ -152,29 +99,21 @@ class SymlinkDataTaskTest extends BaseTaskTest
      */
     public function withAdditionalDirectoriesAndApplicationRootCreatesCorrectLinks()
     {
-        $dataPath = '../../../../shared/Data';
-        $expectedCommands = array(
-            "cd '/releases/current/app/dir'",
-            "{ [ -d {$dataPath}/fileadmin ] || mkdir -p {$dataPath}/fileadmin ; }",
-            "{ [ -d {$dataPath}/uploads ] || mkdir -p {$dataPath}/uploads ; }",
-            "ln -sf {$dataPath}/fileadmin ./fileadmin",
-            "ln -sf {$dataPath}/uploads ./uploads",
-            "{ [ -d '{$dataPath}/pictures' ] || mkdir -p '{$dataPath}/pictures' ; }",
-            "ln -sf '{$dataPath}/pictures' 'pictures'",
-            "{ [ -d '{$dataPath}/test/assets' ] || mkdir -p '{$dataPath}/test/assets' ; }",
-            "ln -sf '../{$dataPath}/test/assets' 'test/assets'",
-        );
-
         $options = array(
             'applicationRootDirectory' => 'app/dir/',
             'directories' => array('pictures', 'test/assets'),
         );
+        $this->task->execute($this->node, $this->application, $this->deployment, $options);
 
-        $this->shellMock->expects($this->once())
-            ->method('executeOrSimulate')
-            ->with($expectedCommands, $this->nodeMock, $this->deploymentMock)
-        ;
-
-        $this->task->execute($this->nodeMock, $this->applicationMock, $this->deploymentMock, $options);
+        $dataPath = '../../../../shared/Data';
+        $this->assertCommandExecuted("cd '{$this->deployment->getApplicationReleasePath($this->application)}/app/dir'");
+        $this->assertCommandExecuted("{ [ -d {$dataPath}/fileadmin ] || mkdir -p {$dataPath}/fileadmin ; }");
+        $this->assertCommandExecuted("{ [ -d {$dataPath}/uploads ] || mkdir -p {$dataPath}/uploads ; }");
+        $this->assertCommandExecuted("ln -sf {$dataPath}/fileadmin ./fileadmin");
+        $this->assertCommandExecuted("ln -sf {$dataPath}/uploads ./uploads");
+        $this->assertCommandExecuted("{ [ -d '{$dataPath}/pictures' ] || mkdir -p '{$dataPath}/pictures' ; }");
+        $this->assertCommandExecuted("ln -sf '{$dataPath}/pictures' 'pictures'");
+        $this->assertCommandExecuted("{ [ -d '{$dataPath}/test/assets' ] || mkdir -p '{$dataPath}/test/assets' ; }");
+        $this->assertCommandExecuted("ln -sf '../{$dataPath}/test/assets' 'test/assets'");
     }
 }
