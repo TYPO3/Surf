d1a06b20e1e8bef9d0548139bcf901f8c05bdf55
Helmut Hummel <info@helhum.io>
2016-03-31 14:25:56 +0200

    Simplify task manager test

--- Tests/Unit/Domain/Service/TaskManagerTest.php
+++ Tests/Unit/Domain/Service/TaskManagerTest.php
@@ -12,33 +12,66 @@ namespace TYPO3\Surf\Tests\Unit\Domain\Service;
 class TaskManagerTest extends \PHPUnit_Framework_TestCase
 {
     /**
+     * @var \TYPO3\Surf\Domain\Model\Task|\PHPUnit_Framework_MockObject_MockObject
+     */
+    protected $task;
+
+    /**
+     * @var \TYPO3\Surf\Domain\Model\Node
+     */
+    protected $node;
+
+    /**
+     * @var \TYPO3\Surf\Domain\Model\Application
+     */
+    protected $application;
+
+    /**
+     * @var \TYPO3\Surf\Domain\Model\Deployment
+     */
+    protected $deployment;
+
+    /**
+     * @var \TYPO3\Surf\Domain\Service\TaskManager|\PHPUnit_Framework_MockObject_MockObject
+     */
+    protected $taskManager;
+
+    protected function setUp()
+    {
+        $this->node = new \TYPO3\Surf\Domain\Model\Node('Test node');
+        $this->application = new \TYPO3\Surf\Domain\Model\Application('Test application');
+        $this->deployment = new \TYPO3\Surf\Domain\Model\Deployment('Test deployment');
+        /** @var \Psr\Log\LoggerInterface $logger */
+        $logger = $this->getMock('Psr\Log\LoggerInterface');
+        $this->deployment->setLogger($logger);
+        $this->task = $this->getMock(\TYPO3\Surf\Domain\Model\Task::class);
+
+        $this->taskManager = $this->getMock(\TYPO3\Surf\Domain\Service\TaskManager::class, array('createTaskInstance'));
+        $this->taskManager
+            ->expects($this->any())
+            ->method('createTaskInstance')
+            ->will($this->returnValue($this->task));
+    }
+
+    /**
      * @test
      */
     public function executePassesPrefixedTaskOptionsToTask()
     {
-        $node = new \TYPO3\Surf\Domain\Model\Node('Test node');
-        $application = new \TYPO3\Surf\Domain\Model\Application('Test application');
-        $deployment = new \TYPO3\Surf\Domain\Model\Deployment('Test deployment');
-        /** @var \Psr\Log\LoggerInterface|\PHPUnit_Framework_MockObject_MockObject $logger */
-        $logger = $this->getMock('Psr\Log\LoggerInterface');
-        $deployment->setLogger($logger);
-        $task = $this->getMock('TYPO3\Surf\Domain\Model\Task');
-        /** @var \TYPO3\Surf\Domain\Service\TaskManager|\PHPUnit_Framework_MockObject_MockObject $taskManager */
-        $taskManager = $this->getMock('TYPO3\Surf\Domain\Service\TaskManager', array('createTaskInstance'));
-        $taskManager->expects($this->any())->method('createTaskInstance')->with('MyVendor\\MyPackage\\Task\\TaskGroup\\MyTask')->will($this->returnValue($task));
-
         $globalOptions = array(
             'MyVendor\\MyPackage\\Task\\TaskGroup\\MyTask[taskOption]' => 'Foo'
         );
-        $deployment->setOptions($globalOptions);
+        $this->deployment->setOptions($globalOptions);
 
-        $task->expects($this->atLeastOnce())->method('execute')->with(
-            $this->anything(), $this->anything(), $this->anything(),
+        $this->task->expects($this->atLeastOnce())->method('execute')->with(
+            $this->anything(),
+            $this->anything(),
+            $this->anything(),
             $this->arrayHasKey('taskOption')
         );
 
         $localOptions = array();
-        $taskManager->execute('MyVendor\\MyPackage\\Task\\TaskGroup\\MyTask', $node, $application, $deployment, 'test', $localOptions);
+        $this->taskManager->execute('MyVendor\\MyPackage\\Task\\TaskGroup\\MyTask', $this->node, $this->application, $this->deployment, 'test', $localOptions);
     }
 
     /**
@@ -46,29 +79,18 @@ class TaskManagerTest extends \PHPUnit_Framework_TestCase
      */
     public function executePassesNodeOptionsToTask()
     {
-        $node = new \TYPO3\Surf\Domain\Model\Node('Test node');
-        $application = new \TYPO3\Surf\Domain\Model\Application('Test application');
-        $deployment = new \TYPO3\Surf\Domain\Model\Deployment('Test deployment');
-        /** @var \Psr\Log\LoggerInterface|\PHPUnit_Framework_MockObject_MockObject $logger */
-        $logger = $this->getMock('Psr\Log\LoggerInterface');
-        $deployment->setLogger($logger);
-        $task = $this->getMock('TYPO3\Surf\Domain\Model\Task');
-        /** @var \TYPO3\Surf\Domain\Service\TaskManager|\PHPUnit_Framework_MockObject_MockObject $taskManager */
-        $taskManager = $this->getMock('TYPO3\Surf\Domain\Service\TaskManager', array('createTaskInstance'));
-        $taskManager->expects($this->any())->method('createTaskInstance')->with('MyVendor\\MyPackage\\Task\\TaskGroup\\MyTask')->will($this->returnValue($task));
-
         $nodeOptions = array(
             'ssh[username]' => 'jdoe'
         );
-        $node->setOptions($nodeOptions);
+        $this->node->setOptions($nodeOptions);
 
-        $task->expects($this->atLeastOnce())->method('execute')->with(
+        $this->task->expects($this->atLeastOnce())->method('execute')->with(
             $this->anything(), $this->anything(), $this->anything(),
             $this->arrayHasKey('ssh[username]')
         );
 
         $localOptions = array();
-        $taskManager->execute('MyVendor\\MyPackage\\Task\\TaskGroup\\MyTask', $node, $application, $deployment, 'test', $localOptions);
+        $this->taskManager->execute('MyVendor\\MyPackage\\Task\\TaskGroup\\MyTask', $this->node, $this->application, $this->deployment, 'test', $localOptions);
     }
 
     /**
@@ -76,28 +98,17 @@ class TaskManagerTest extends \PHPUnit_Framework_TestCase
      */
     public function executePassesApplicationOptionsToTask()
     {
-        $node = new \TYPO3\Surf\Domain\Model\Node('Test node');
-        $application = new \TYPO3\Surf\Domain\Model\Application('Test application');
-        $deployment = new \TYPO3\Surf\Domain\Model\Deployment('Test deployment');
-        /** @var \Psr\Log\LoggerInterface|\PHPUnit_Framework_MockObject_MockObject $logger */
-        $logger = $this->getMock('Psr\Log\LoggerInterface');
-        $deployment->setLogger($logger);
-        $task = $this->getMock('TYPO3\Surf\Domain\Model\Task');
-        /** @var \TYPO3\Surf\Domain\Service\TaskManager|\PHPUnit_Framework_MockObject_MockObject $taskManager */
-        $taskManager = $this->getMock('TYPO3\Surf\Domain\Service\TaskManager', array('createTaskInstance'));
-        $taskManager->expects($this->any())->method('createTaskInstance')->with('MyVendor\\MyPackage\\Task\\TaskGroup\\MyTask')->will($this->returnValue($task));
-
         $applicationOptions = array(
             'repositoryUrl' => 'ssh://review.typo3.org/foo'
         );
-        $application->setOptions($applicationOptions);
+        $this->application->setOptions($applicationOptions);
 
-        $task->expects($this->atLeastOnce())->method('execute')->with(
+        $this->task->expects($this->atLeastOnce())->method('execute')->with(
             $this->anything(), $this->anything(), $this->anything(),
             $this->arrayHasKey('repositoryUrl')
         );
 
         $localOptions = array();
-        $taskManager->execute('MyVendor\\MyPackage\\Task\\TaskGroup\\MyTask', $node, $application, $deployment, 'test', $localOptions);
+        $this->taskManager->execute('MyVendor\\MyPackage\\Task\\TaskGroup\\MyTask', $this->node, $this->application, $this->deployment, 'test', $localOptions);
     }
 }
