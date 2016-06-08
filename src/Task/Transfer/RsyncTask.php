b903a69022af604341a7ee42307d9c792059a510
Helmut Hummel <info@helhum.io>
2016-06-08 18:11:08 +0200

    [BUGFIX] Fix expect script also for rsync task

--- src/Task/Transfer/RsyncTask.php
+++ src/Task/Transfer/RsyncTask.php
@@ -6,6 +6,7 @@ namespace TYPO3\Surf\Task\Transfer;
  *                                                                        *
  *                                                                        */
 
+use TYPO3\Flow\Utility\Files;
 use TYPO3\Surf\Domain\Model\Application;
 use TYPO3\Surf\Domain\Model\Deployment;
 use TYPO3\Surf\Domain\Model\Node;
@@ -33,7 +34,7 @@ class RsyncTask extends \TYPO3\Surf\Domain\Model\Task implements \TYPO3\Surf\Dom
         $localPackagePath = $deployment->getWorkspacePath($application);
         $releasePath = $deployment->getApplicationReleasePath($application);
 
-        $remotePath = $application->getDeploymentPath() . '/cache/transfer';
+        $remotePath = Files::concatenatePaths(array($application->getDeploymentPath(), 'cache/transfer'));
         // make sure there is a remote .cache folder
         $command = 'mkdir -p ' . $remotePath;
         $this->shell->executeOrSimulate($command, $node, $deployment);
@@ -55,14 +56,22 @@ class RsyncTask extends \TYPO3\Surf\Domain\Model\Task implements \TYPO3\Surf\Dom
         $command = "rsync {$quietFlag} --compress {$rshFlag} {$rsyncFlags} " . escapeshellarg($localPackagePath . '/.') . ' ' . escapeshellarg($destinationArgument);
 
         if ($node->hasOption('password')) {
-            $resourcesPath = realpath(__DIR__ . '/../../../Resources');
-            $passwordSshLoginScriptPathAndFilename = $resourcesPath . '/Private/Scripts/PasswordSshLogin.expect';
+            $passwordSshLoginScriptPathAndFilename = Files::concatenatePaths(array(dirname(dirname(dirname(__DIR__))), 'Resources', 'Private/Scripts/PasswordSshLogin.expect'));
+            if (\Phar::running() !== '') {
+                $passwordSshLoginScriptContents = file_get_contents($passwordSshLoginScriptPathAndFilename);
+                $passwordSshLoginScriptPathAndFilename = Files::concatenatePaths(array($deployment->getTemporaryPath(), 'PasswordSshLogin.expect'));
+                file_put_contents($passwordSshLoginScriptPathAndFilename, $passwordSshLoginScriptContents);
+            }
             $command = sprintf('expect %s %s %s', escapeshellarg($passwordSshLoginScriptPathAndFilename), escapeshellarg($node->getOption('password')), $command);
         }
 
         $localhost = new Node('localhost');
         $localhost->setHostname('localhost');
         $this->shell->executeOrSimulate($command, $localhost, $deployment);
+
+        if (isset($passwordSshLoginScriptPathAndFilename) && \Phar::running() !== '') {
+            unlink($passwordSshLoginScriptPathAndFilename);
+        }
 
         $command = strtr("cp -RPp $remotePath/. $releasePath", "\t\n", '  ');
         // TODO Copy revision file (if it exists) for application to deployment path with release identifier
