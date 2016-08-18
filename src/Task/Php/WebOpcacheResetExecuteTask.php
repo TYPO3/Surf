9cb156f38945d89ce6976e94dd1def5cf28d57e1
Max Strübing <struebing@sitegeist.de>
2016-08-18 11:21:22 +0200

    [TASK] Let WebOpcacheResetExecuteTask throw an error
    to be able to rollback

--- src/Task/Php/WebOpcacheResetExecuteTask.php
+++ src/Task/Php/WebOpcacheResetExecuteTask.php
@@ -40,7 +40,7 @@ class WebOpcacheResetExecuteTask extends \TYPO3\Surf\Domain\Model\Task
 
         $result = file_get_contents($scriptUrl);
         if ($result !== 'success') {
-            $deployment->getLogger()->warning('Executing PHP opcache reset script at "' . $scriptUrl . '" did not return expected result');
+            throw new \TYPO3\Surf\Exception\TaskExecutionException('WebOpcacheResetExecuteTask at "' . $scriptUrl . '" did not return expected result', 1471511860);
         }
     }
 }
