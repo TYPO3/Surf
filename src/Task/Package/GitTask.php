2a1e3eefef874a7d0a1d7026fabf5fddd5be71a5
Daniela Grammlich <grammlich@punkt.de>
2016-09-19 15:57:36 +0200

    [TASK] Add documentation in class annotation for `GitTask` (#33)

--- src/Task/Package/GitTask.php
+++ src/Task/Package/GitTask.php
@@ -15,10 +15,34 @@ use TYPO3\Surf\Exception\InvalidConfigurationException;
 use TYPO3\Surf\Task\Git\AbstractCheckoutTask;
 
 /**
- * A Git package task
+ * A Git package task.
  *
  * Package an application by doing a local git update / clone before using the configured "transferMethod" to transfer
  * assets to the application node(s).
+ *
+ * It takes the following options:
+ *
+ * * repositoryUrl - The git remote to use.
+ * * fetchAllTags (optional) - If true, make a fetch on tags.
+ * * gitPostCheckoutCommands (optional) - An array with commands to execute after checkout.
+ * * hardClean (optional) - If true, execute a hard clean.
+ * * recursiveSubmodules (optional) - If true, handle submodules recursive.
+ * * verbose (optional) - If true, output verbose information from git.
+ *
+ * Example:
+ *  $workflow
+ *      ->setTaskOptions('TYPO3\Surf\Task\Package\GitTask', [
+ *                  'repositoryUrl' => 'git@github.com:TYPO3/Surf.git',
+ *                  'verbose' => true,
+ *                  'recursiveSubmodules' => true,
+ *                  'fetchAllTags' => true,
+ *                  'hardClean' => true,
+ *                  'gitPostCheckoutCommands' => [
+ *                      '/var/www/outerspace' => 'composer install'
+ *                  ]
+ *              ]
+ *          ]
+ *      );
  */
 class GitTask extends AbstractCheckoutTask
 {
