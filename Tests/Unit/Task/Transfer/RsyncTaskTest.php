cf20b8ddb99453972f8a5d0eb775e915abcc4cb4
Florian Kaiser <florian.kaiser@fnkr.net>
2017-05-24 09:10:58 +0200

    Disable PubkeyAuthentication if password is provided

--- Tests/Unit/Task/Transfer/RsyncTaskTest.php
+++ Tests/Unit/Task/Transfer/RsyncTaskTest.php
@@ -45,6 +45,22 @@ class RsyncTaskTest extends BaseTaskTest
     /**
      * @test
      */
+    public function executeWithUsernameAndPasswordAndDefaultOptionsCreatesDirectoryAndTransfersAndCopiesFiles()
+    {
+        $this->node->setOption('hostname', 'myserver.local');
+        $this->node->setOption('username', 'jdoe');
+        $this->node->setOption('password', 'jdoe');
+
+        $this->task->execute($this->node, $this->application, $this->deployment, array());
+
+        $this->assertCommandExecuted('mkdir -p /home/jdoe/app/cache/transfer');
+        $this->assertCommandExecuted('/rsync -q --compress --rsh="ssh -o PubkeyAuthentication=no"  --recursive --times --perms --links --delete --delete-excluded --exclude \'.git\' \'.*\/Data\/Surf\/TestDeployment\/TestApplication\/.\' \'jdoe@myserver.local:\/home\/jdoe\/app\/cache\/transfer\'/');
+        $this->assertCommandExecuted('/cp -RPp \/home\/jdoe\/app\/cache\/transfer\/. \/home\/jdoe\/app\/releases\/[0-9]+/');
+    }
+
+    /**
+     * @test
+     */
     public function executeWithPrivateKeyAddsFlagToSshCommand()
     {
         $this->node->setOption('hostname', 'myserver.local');
