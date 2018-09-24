2db813fb5a21c4e4beb637f150f0ed451de76dfa
Sebastian Schreiber <breakpoint@schreibersebastian.de>
2018-09-24 23:19:48 +0200

    [TASK] Add travis push back task for the documentation

---
+++ .travis/push-documentation.sh
@@ -0,0 +1,26 @@
+#!/bin/sh
+
+setup_git() {
+  git config --global user.email "travis@travis-ci.org"
+  git config --global user.name "Travis CI"
+}
+
+compile() {
+    ./../render_phpdocs.sh
+}
+
+commit_website_files() {
+  git checkout -b documentation
+  git add Documentation
+  git commit --message "Travis build: $TRAVIS_BUILD_NUMBER"
+}
+
+upload_files() {
+  git remote add origin https://${GH_TOKEN}@github.com/TYPO3/Surf.git > /dev/null 2>&1
+  git push --quiet --set-upstream origin documentation
+}
+
+setup_git
+compile
+commit_website_files
+upload_files
