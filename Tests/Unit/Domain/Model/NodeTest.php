ef7fe577d36883d16b051df9de57ecc7cb845ae3
Sebastian Schreiber <breakpoint@schreibersebastian.de>
2018-09-24 14:56:13 +0200

    [TASK] Add some simple api methods to configure node

---
+++ Tests/Unit/Domain/Model/NodeTest.php
@@ -0,0 +1,50 @@
+<?php
+
+namespace TYPO3\Surf\Tests\Unit\Domain\Model;
+
+/*
+ * This file is part of TYPO3 Surf.
+ *
+ * For the full copyright and license information, please view the LICENSE.txt
+ * file that was distributed with this source code.
+ */
+
+use TYPO3\Surf\Domain\Model\Node;
+use PHPUnit\Framework\TestCase;
+
+class NodeTest extends TestCase
+{
+
+    /**
+     * @test
+     */
+    public function isLocalhost()
+    {
+        $node = new Node('Node');
+        $node->onLocalhost();
+        $this->assertTrue($node->isLocalhost());
+    }
+
+    /**
+     * @test
+     */
+    public function setPort()
+    {
+        $node = new Node('Node');
+        $node->setPort(222);
+        $this->assertEquals(222, $node->getPort());
+    }
+
+    /**
+     * @test
+     */
+    public function setUsername()
+    {
+        $node = new Node('Node');
+        $node->setUsername('username');
+        $this->assertEquals('username', $node->getUsername());
+    }
+
+}
+
+
