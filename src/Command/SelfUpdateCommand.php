79518e87e9a17104d944aa8303922e109f04d62a
Sebastian Schreiber <breakpoint@schreibersebastian.de>
2018-10-28 13:23:30 +0100

    [TASK] Optimize build process for phar file
    - Create phar file with the box project
    - Update documentation for it

--- src/Command/SelfUpdateCommand.php
+++ src/Command/SelfUpdateCommand.php
@@ -1,4 +1,5 @@
 <?php
+
 namespace TYPO3\Surf\Command;
 
 /*
@@ -10,19 +11,47 @@ namespace TYPO3\Surf\Command;
 
 use Humbug\SelfUpdate\Strategy\GithubStrategy;
 use Humbug\SelfUpdate\Updater;
+use Phar;
 use Symfony\Component\Console\Command\Command;
 use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Input\InputOption;
 use Symfony\Component\Console\Output\OutputInterface;
-use TYPO3\Surf\Integration\FactoryAwareInterface;
-use TYPO3\Surf\Integration\FactoryAwareTrait;
+use Symfony\Component\Console\Style\SymfonyStyle;
 
 /**
  * Surf list command
  */
-class SelfUpdateCommand extends Command implements FactoryAwareInterface
+class SelfUpdateCommand extends Command
 {
-    use FactoryAwareTrait;
+
+    /**
+     * @var Updater
+     */
+    private $updater;
+
+    /**
+     * SelfUpdateCommand constructor.
+     *
+     * @param null $name
+     */
+    public function __construct($name = null)
+    {
+        parent::__construct($name);
+
+        $this->updater = new Updater(null, false, Updater::STRATEGY_GITHUB);
+        /** @var GithubStrategy $strategy */
+        $strategy = $this->updater->getStrategy();
+        $strategy->setPackageName('TYPO3/Surf');
+        $strategy->setPharName('surf.phar');
+    }
+
+    /**
+     * @return bool
+     */
+    public function isEnabled()
+    {
+        return Phar::running() !== '';
+    }
 
     /**
      * Configure
@@ -30,12 +59,12 @@ class SelfUpdateCommand extends Command implements FactoryAwareInterface
     protected function configure()
     {
         $this->setName('self-update')
-            ->addOption(
-                'stability',
-                null,
-                InputOption::VALUE_OPTIONAL,
-                'GitHub stability value (' . GithubStrategy::STABLE . ', ' . GithubStrategy::UNSTABLE . ', ' . GithubStrategy::ANY . ')'
-            )->addOption(
+             ->addOption(
+                 'stability',
+                 null,
+                 InputOption::VALUE_OPTIONAL,
+                 'GitHub stability value (' . GithubStrategy::STABLE . ', ' . GithubStrategy::UNSTABLE . ', ' . GithubStrategy::ANY . ')'
+             )->addOption(
                 'check',
                 null,
                 InputOption::VALUE_NONE,
@@ -45,15 +74,7 @@ class SelfUpdateCommand extends Command implements FactoryAwareInterface
                 null,
                 InputOption::VALUE_NONE,
                 'Rolls back to previous version'
-            );
-    }
-
-    /**
-     * @return bool
-     */
-    public function isEnabled()
-    {
-        return \Phar::running() !== '';
+            )->setDescription(sprintf('Update %s to most recent stable build', $this->getLocalPharName()));
     }
 
     /**
@@ -64,40 +85,57 @@ class SelfUpdateCommand extends Command implements FactoryAwareInterface
      */
     protected function execute(InputInterface $input, OutputInterface $output)
     {
-        $updater = new Updater(null, false);
-        $updater->setStrategy(Updater::STRATEGY_GITHUB);
-        $strategy = $updater->getStrategy();
-        /* @var GithubStrategy $strategy */
-        $strategy->setPackageName('TYPO3/Surf');
-        $strategy->setPharName('surf.phar');
-        $strategy->setCurrentLocalVersion($this->getApplication()->getVersion());
+        $io = new SymfonyStyle($input, $output);
 
         $stability = $input->getOption('stability');
         if (empty($stability)) {
             // Unstable by default. Should be removed once we have a 2.0.0 final
             $stability = GithubStrategy::UNSTABLE;
         }
+        /** @var GithubStrategy $strategy */
+        $strategy = $this->updater->getStrategy();
+        $strategy->setCurrentLocalVersion($this->getApplication()->getVersion());
         $strategy->setStability($stability);
 
         if ($input->getOption('check')) {
-            $result = $updater->hasUpdate();
+            $result = $this->updater->hasUpdate();
             if ($result) {
                 $output->writeln(sprintf(
                     'The %s build available remotely is: %s',
                     $strategy->getStability() === GithubStrategy::ANY ? 'latest' : 'current ' . $strategy->getStability(),
-                    $updater->getNewVersion()
+                    $this->updater->getNewVersion()
                 ));
-            } elseif (false === $updater->getNewVersion()) {
+            } elseif (false === $this->updater->getNewVersion()) {
                 $output->writeln('There are no new builds available.');
             } else {
                 $output->writeln(sprintf('You have the current %s build installed.', $strategy->getStability()));
             }
         } elseif ($input->getOption('rollback')) {
-            $result = $updater->rollback();
+            $result = $this->updater->rollback();
             $result ? $output->writeln('Success!') : $output->writeln('Failure!');
         } else {
-            $result = $updater->update();
-            $result ? $output->writeln('Updated.') : $output->writeln('No update needed!');
+            $result = $this->updater->update();
+
+            if ($result) {
+                $io->success(
+                    sprintf(
+                        'Your %s has been updated from "%s" to "%s".',
+                        $this->getLocalPharName(),
+                        $this->updater->getOldVersion(),
+                        $this->updater->getNewVersion()
+                    )
+                );
+            } else {
+                $io->success(sprintf('Your %s is already up to date.', $this->getLocalPharName()));
+            }
         }
+    }
+
+    /**
+     * @return string
+     */
+    private function getLocalPharName()
+    {
+        return basename(Phar::running());
     }
 }
