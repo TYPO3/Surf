b99340559821b693c77361cb02854472ffabdc0c
Sebastian Schreiber <breakpoint@schreibersebastian.de>
2020-02-03 14:52:57 +0100

    [TASK] Keep releases by Age (#184)
    * [TASK] Keep releases by Age
    
    - Resolves #81

---
+++ src/Domain/Clock/SystemClock.php
@@ -0,0 +1,59 @@
+<?php
+
+namespace TYPO3\Surf\Domain\Clock;
+
+/*
+ * This file is part of TYPO3 Surf.
+ *
+ * For the full copyright and license information, please view the LICENSE.txt
+ * file that was distributed with this source code.
+ */
+
+use DateTime;
+
+final class SystemClock implements ClockInterface
+{
+    /**
+     * @return int
+     */
+    public function currentTime()
+    {
+        return time();
+    }
+
+    /**
+     * @param string $string
+     * @param int $time
+     *
+     * @return false|int
+     * @throws ClockException
+     */
+    public function stringToTime($string, $time = null)
+    {
+        $time = isset($time) ? $time : time();
+        $timestamp = strtotime($string, $time);
+
+        if ($timestamp === false) {
+            throw ClockException::stringCouldNotBeConvertedToTimestamp($string);
+        }
+
+        return $timestamp;
+    }
+
+    /**
+     * @param string $format
+     * @param string $time
+     *
+     * @return int
+     * @throws ClockException
+     */
+    public function createTimestampFromFormat($format, $time)
+    {
+        $datetime = DateTime::createFromFormat($format, $time);
+
+        if ($datetime === false) {
+            throw ClockException::formatCouldNotBeConvertedToTimestamp($format, $time);
+        }
+        return $datetime->format('U');
+    }
+}
