3f1c9a278210f1bdbfc78ec2cb85988bd1519098
Sebastian Schreiber <breakpoint@schreibersebastian.de>
2020-03-10 09:58:30 +0100

    [TASK] Add unit test for SymlinkReleaseTask (#371)

---
+++ tests/Unit/Task/SymlinkReleaseTaskTest.php
@@ -0,0 +1,59 @@
+<?php
+
+namespace TYPO3\Surf\Tests\Unit\Task;
+
+/*
+ * This file is part of TYPO3 Surf.
+ *
+ * For the full copyright and license information, please view the LICENSE.txt
+ * file that was distributed with this source code.
+ */
+
+use TYPO3\Surf\Task\SymlinkReleaseTask;
+
+class SymlinkReleaseTaskTest extends BaseTaskTest
+{
+
+    /**
+     * @test
+     */
+    public function executeSuccessfully(): void
+    {
+        $this->task->execute($this->node, $this->application, $this->deployment);
+        $this->assertCommandExecuted($this->expectedCommand());
+    }
+
+    /**
+     * @test
+     */
+    public function simulateSuccessfully(): void
+    {
+        $this->task->simulate($this->node, $this->application, $this->deployment);
+        $this->assertCommandExecuted($this->expectedCommand());
+    }
+
+    /**
+     * @test
+     */
+    public function rollbackSuccessfully(): void
+    {
+        $this->task->rollback($this->node, $this->application, $this->deployment);
+        $this->assertCommandExecuted('cd ' . $this->application->getReleasesPath() . ' && rm -f ./current && mv ./previous ./current');
+    }
+
+    /**
+     * @return SymlinkReleaseTask
+     */
+    protected function createTask()
+    {
+        return new SymlinkReleaseTask();
+    }
+
+    /**
+     * @return string
+     */
+    private function expectedCommand(): string
+    {
+        return 'cd ' . $this->application->getReleasesPath() . ' && rm -f ./previous && if [ -e ./current ]; then mv ./current ./previous; fi && ln -s ./' . $this->deployment->getReleaseIdentifier() . ' ./current && rm -f ./next';
+    }
+}
