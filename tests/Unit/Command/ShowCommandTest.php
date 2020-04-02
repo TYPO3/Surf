2ef422c261404477676ec15faf9167835f6863cd
Sebastian Schreiber <breakpoint@schreibersebastian.de>
2020-04-02 22:00:00 +0200

    [TASK] Test ShowCommand

---
+++ tests/Unit/Command/ShowCommandTest.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace TYPO3\Surf\Tests\Unit\Command;
+
+/*
+ * This file is part of TYPO3 Surf.
+ *
+ * For the full copyright and license information, please view the LICENSE.txt
+ * file that was distributed with this source code.
+ */
+
+use Prophecy\Argument;
+use Symfony\Component\Console\Tester\CommandTester;
+use TYPO3\Surf\Command\DescribeCommand;
+use TYPO3\Surf\Command\ShowCommand;
+use PHPUnit\Framework\TestCase;
+use TYPO3\Surf\Integration\FactoryInterface;
+
+class ShowCommandTest extends TestCase
+{
+    /**
+     * @test
+     */
+    public function executeSuccessfully(): void
+    {
+        $factory = $this->prophesize(FactoryInterface::class);
+        $factory->getDeploymentNames('.surf')->willReturn(['foo', 'bar', 'baz']);
+        $factory->getDeploymentsBasePath('.surf')->willReturn('./surf');
+        $command = new ShowCommand($factory->reveal());
+        $commandTester = new CommandTester($command);
+        $commandTester->execute(['--configurationPath' => '.surf']);
+
+        $this->assertEquals('
+<u>Deployments in "./surf":</u>
+
+  foo
+  bar
+  baz
+
+', $commandTester->getDisplay());
+    }
+}
