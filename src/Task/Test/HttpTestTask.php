f7302d76c9e9d43fed7f6e2dcacaf9fd4d2e2d60
Simon Schaufelberger <simonschaufi@users.noreply.github.com>
2020-05-30 19:56:58 +0200

    [FEATURE] Add relativeProjectRootPath to deployment (#403)

--- src/Task/Test/HttpTestTask.php
+++ src/Task/Test/HttpTestTask.php
@@ -9,6 +9,7 @@ namespace TYPO3\Surf\Task\Test;
  * file that was distributed with this source code.
  */
 
+use GuzzleHttp\Client;
 use GuzzleHttp\ClientInterface;
 use GuzzleHttp\Exception\GuzzleException;
 use GuzzleHttp\Exception\RequestException;
