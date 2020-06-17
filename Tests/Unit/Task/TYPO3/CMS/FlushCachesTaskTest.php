4ab4093cc3156e3299ffe22b500f57f8f55aeca7
Jan Kiesewetter <jan@t3easy.de>
2020-06-17 21:09:08 +0200

    [TASK] Add test for FlushCachesTask

---
+++ Tests/Unit/Task/TYPO3/CMS/FlushCachesTaskTest.php
@@ -0,0 +1,51 @@
+<?php
+namespace TYPO3\Surf\Tests\Unit\Task\TYPO3\CMS;
+
+/*
+ * This file is part of TYPO3 Surf.
+ *
+ * For the full copyright and license information, please view the LICENSE.txt
+ * file that was distributed with this source code.
+ */
+
+use TYPO3\Surf\Application\TYPO3\CMS;
+use TYPO3\Surf\Task\TYPO3\CMS\FlushCachesTask;
+use TYPO3\Surf\Tests\Unit\Task\BaseTaskTest;
+
+/**
+ * Class SetUpExtensionsTaskTest
+ */
+class FlushCachesTaskTest extends BaseTaskTest
+{
+    /**
+     * @var FlushCachesTask
+     */
+    protected $task;
+
+    /**
+     * @return FlushCachesTask
+     */
+    protected function createTask()
+    {
+        return new FlushCachesTask();
+    }
+
+    protected function setUp()
+    {
+        parent::setUp();
+        $this->application = new CMS('TestApplication');
+        $this->application->setDeploymentPath('/home/jdoe/app');
+    }
+
+    /**
+     * @test
+     */
+    public function executeWithoutOptionExecutesCacheFlushWithForceParameter()
+    {
+        $options = [
+            'scriptFileName' => 'vendor/bin/typo3cms'
+        ];
+        $this->task->execute($this->node, $this->application, $this->deployment, $options);
+        $this->assertCommandExecuted("/php 'vendor\/bin\/typo3cms' 'cache:flush' '--force'$/");
+    }
+}
