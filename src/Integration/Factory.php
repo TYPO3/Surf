6f2c2d13ba6f0ef321947ce87a461b8206d0164b
Niklas Droste <Nikdro@users.noreply.github.com>
2021-03-25 14:46:50 +0100

    BUGFIX: Also check for deploy-config  in home-dir

--- src/Integration/Factory.php
+++ src/Integration/Factory.php
@@ -147,11 +147,15 @@ class Factory implements FactoryInterface, ContainerAwareInterface
 
         $deploymentPathAndFilename = Files::concatenatePaths([$deploymentConfigurationPath, $deploymentName . '.php']);
         if (! $this->filesystem->fileExists($deploymentPathAndFilename)) {
-            $this->logger->error(sprintf("The deployment file %s does not exist.\n", $deploymentPathAndFilename));
-            $deployment = new FailedDeployment();
-            $deployment->setLogger($this->logger);
-
-            return $deployment;
+           
+            //Check if file exists in home-dir configurations instead
+            $deploymentPathAndFilename = $deploymentPathAndFilename = Files::concatenatePaths([$this->getHomeDirectory(), 'deployments', $deploymentName . '.php']);
+            if (! $this->filesystem->fileExists($deploymentPathAndFilename)) {
+                $this->logger->error(sprintf("The deployment file %s does not exist.\n", $deploymentPathAndFilename));
+                $deployment = new FailedDeployment();
+                $deployment->setLogger($this->logger);
+                return $deployment;
+            }
         }
 
         $deployment = new Deployment($deploymentName);
