cf1236e1f73caa069d47cfd637484af90ddb3675
Markus <markus.zipfel@aimcom.de>
2021-11-22 12:52:30 +0100

    [BUGFIX] Deployment of TYPO3 11.5 with TYPO3 Console 7.0
    Added version check and condition to make sure that extension setup still works with older TYPO3 Console versions
    
    Related: #452

--- src/Task/TYPO3/CMS/SetUpExtensionsTask.php
+++ src/Task/TYPO3/CMS/SetUpExtensionsTask.php
@@ -30,6 +30,7 @@ class SetUpExtensionsTask extends AbstractCliTask
 
         try {
             $scriptFileName = $this->getConsoleScriptFileName($node, $application, $deployment, $options);
+            $consoleVersion = $this->getConsoleVersion($scriptFileName, $node, $application, $deployment, $options);
         } catch (InvalidConfigurationException $e) {
             $deployment->getLogger()->warning('TYPO3 Console script (' . $options['scriptFileName'] . ') was not found! Make sure it is available in your project, you set the "scriptFileName" option correctly or remove this task (' . __CLASS__ . ') in your deployment configuration!');
             return;
@@ -38,13 +39,27 @@ class SetUpExtensionsTask extends AbstractCliTask
         $options = $this->configureOptions($options);
 
         $commandArguments = [$scriptFileName];
-        if (empty($options['extensionKeys'])) {
-            $commandArguments[] = 'extension:setupactive';
-        } else {
+        
+        // TYPO3 Console behaviour changed since version 7:
+        // Version <7: `typo3cms extension:setupactive` was called to install all extensions
+        // And `typo3cms extension:setup extension_key_1,extension_key_2` was called to install some extensions by their key
+        // Version >=7: `typo3cms extension:setup` is called to install all extensions
+        // `typo3cms extension:setup -e extension_key_1 -e extension_key_2` is called to install these two extensions
+
+        if ((int)$consoleVersion >= 7) {
             $commandArguments[] = 'extension:setup';
-            foreach ($options['extensionKeys'] as $extensionKey) {
-                $commandArguments[] = '-e';
-                $commandArguments[] = $extensionKey;
+            if (!empty($options['extensionKeys'])) {
+                foreach ($options['extensionKeys'] as $extensionKey) {
+                    $commandArguments[] = '-e';
+                    $commandArguments[] = $extensionKey;
+                }
+            }
+        } else {
+            if (empty($options['extensionKeys'])) {
+                $commandArguments[] = 'extension:setupactive';
+            } else {
+                $commandArguments[] = 'extension:setup';
+                $commandArguments[] = implode(',', $options['extensionKeys']);
             }
         }
 
@@ -61,5 +76,33 @@ class SetUpExtensionsTask extends AbstractCliTask
     {
         $resolver->setDefault('extensionKeys', []);
         $resolver->setAllowedTypes('extensionKeys', 'array');
+    }
+
+    /**
+     * Get TYPO3 Console version string, e.g. "7.0.0"
+     *
+     * @param string $scriptFileName
+     * @param \TYPO3\Surf\Domain\Model\Node $node
+     * @param CMS|\TYPO3\Surf\Domain\Model\Application $application
+     * @param \TYPO3\Surf\Domain\Model\Deployment $deployment
+     * @param array $options
+     * @return string Returns empty string ("") if version could not be determined
+     */
+    protected function getConsoleVersion(
+        string $scriptFileName,
+        \TYPO3\Surf\Domain\Model\Node $node,
+        \TYPO3\Surf\Domain\Model\Application $application,
+        \TYPO3\Surf\Domain\Model\Deployment $deployment,
+        array $options
+    ): string {
+        $consoleVersionCommandOutput = $this->executeCliCommand(
+            [$scriptFileName, '--version'],
+            $node,
+            $application,
+            $deployment,
+            $options
+        );
+        $consoleVersionLine = explode(PHP_EOL, $consoleVersionCommandOutput)[0] ?? '';
+        return str_replace('TYPO3 Console ', '', $consoleVersionLine);
     }
 }
