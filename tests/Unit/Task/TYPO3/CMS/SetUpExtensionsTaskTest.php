d13f074e92ed721184d4062447746fe59047d264
Markus <markus.zipfel@aimcom.de>
2021-11-23 10:40:20 +0100

    [TASK] Update unit test for deployment of TYPO3 11.5 with TYPO3 Console 7.0
    Updated `SetUpExtensionsTaskTest.php` to test for console versions <7 and >=7
    
    Related: #452

--- tests/Unit/Task/TYPO3/CMS/SetUpExtensionsTaskTest.php
+++ tests/Unit/Task/TYPO3/CMS/SetUpExtensionsTaskTest.php
@@ -42,12 +42,18 @@ class SetUpExtensionsTaskTest extends BaseTaskTest
      */
     public function executeWithoutOptionExecutesSetUpActive(): void
     {
-        $this->task->execute(
-            $this->node,
-            $this->application,
-            $this->deployment,
-            ['scriptFileName' => 'vendor/bin/typo3cms']
-        );
+        $mockTask = $this->getMockTaskForConsoleVersion('7.0.0');
+        $mockTask->execute($this->node, $this->application, $this->deployment, ['scriptFileName' => 'vendor/bin/typo3cms']);
+        $this->assertCommandExecuted("php 'vendor/bin/typo3cms' 'extension:setup'");
+    }
+
+    /**
+     * @test
+     */
+    public function executeWithoutOptionExecutesSetUpActiveForOldConsoleVersion(): void
+    {
+        $mockTask = $this->getMockTaskForConsoleVersion('6.9.9');
+        $mockTask->execute($this->node, $this->application, $this->deployment, ['scriptFileName' => 'vendor/bin/typo3cms']);
         $this->assertCommandExecuted("php 'vendor/bin/typo3cms' 'extension:setupactive'");
     }
 
@@ -60,7 +66,24 @@ class SetUpExtensionsTaskTest extends BaseTaskTest
             'scriptFileName' => 'vendor/bin/typo3cms',
             'extensionKeys' => ['foo', 'bar']
         ];
-        $this->task->execute($this->node, $this->application, $this->deployment, $options);
+
+        $mockTask = $this->getMockTaskForConsoleVersion('7.0.0');
+        $mockTask->execute($this->node, $this->application, $this->deployment, $options);
+        $this->assertCommandExecuted("php 'vendor/bin/typo3cms' 'extension:setup' '-e' 'foo' '-e' 'bar'");
+    }
+
+    /**
+     * @test
+     */
+    public function executeWithOptionExecutesSetUpWithOptionForOldConsoleVersion(): void
+    {
+        $options = [
+            'scriptFileName' => 'vendor/bin/typo3cms',
+            'extensionKeys' => ['foo', 'bar']
+        ];
+
+        $mockTask = $this->getMockTaskForConsoleVersion('6.9.9');
+        $mockTask->execute($this->node, $this->application, $this->deployment, $options);
         $this->assertCommandExecuted("php 'vendor/bin/typo3cms' 'extension:setup' 'foo,bar'");
     }
 
@@ -73,7 +96,25 @@ class SetUpExtensionsTaskTest extends BaseTaskTest
             'scriptFileName' => 'vendor/bin/typo3cms',
             'extensionKeys' => ['foo', 'bar']
         ];
-        $this->task->execute($this->node, $this->application, $this->deployment, $options);
+
+        $mockTask = $this->getMockTaskForConsoleVersion('7.0.0');
+        $mockTask->execute($this->node, $this->application, $this->deployment, $options);
+        $this->assertCommandExecuted("cd '{$this->deployment->getApplicationReleasePath($this->application)}'");
+        $this->assertCommandExecuted("php 'vendor/bin/typo3cms' 'extension:setup' '-e' 'foo' '-e' 'bar'");
+    }
+
+    /**
+     * @test
+     */
+    public function consoleIsFoundInCorrectPathWithoutAppDirectoryForOldConsoleVersion(): void
+    {
+        $options = [
+            'scriptFileName' => 'vendor/bin/typo3cms',
+            'extensionKeys' => ['foo', 'bar']
+        ];
+
+        $mockTask = $this->getMockTaskForConsoleVersion('6.9.9');
+        $mockTask->execute($this->node, $this->application, $this->deployment, $options);
         $this->assertCommandExecuted("cd '{$this->deployment->getApplicationReleasePath($this->application)}'");
         $this->assertCommandExecuted("php 'vendor/bin/typo3cms' 'extension:setup' 'foo,bar'");
     }
@@ -88,11 +129,77 @@ class SetUpExtensionsTaskTest extends BaseTaskTest
             'scriptFileName' => 'vendor/bin/typo3cms',
             'webDirectory' => '/web/',
         ];
-        $this->task->execute($this->node, $this->application, $this->deployment, $options);
+
+        $mockTask = $this->getMockTaskForConsoleVersion('7.0.0');
+        $mockTask->execute($this->node, $this->application, $this->deployment, $options);
+        $this->assertCommandExecuted("cd '{$this->deployment->getApplicationReleasePath($this->application)}'");
+        $this->assertCommandExecuted(
+            "test -f '{$this->deployment->getApplicationReleasePath($this->application)}/vendor/bin/typo3cms'"
+        );
+        $this->assertCommandExecuted("php 'vendor/bin/typo3cms' 'extension:setup' '-e' 'foo' '-e' 'bar'");
+    }
+
+    /**
+     * @test
+     */
+    public function consoleIsFoundInCorrectPathWithWebDirectoryAndSlashesAreTrimmedForOldConsoleVersion(): void
+    {
+        $options = [
+            'extensionKeys' => ['foo', 'bar'],
+            'scriptFileName' => 'vendor/bin/typo3cms',
+            'webDirectory' => '/web/',
+        ];
+
+        $mockTask = $this->getMockTaskForConsoleVersion('6.9.9');
+        $mockTask->execute($this->node, $this->application, $this->deployment, $options);
         $this->assertCommandExecuted("cd '{$this->deployment->getApplicationReleasePath($this->application)}'");
         $this->assertCommandExecuted(
             "test -f '{$this->deployment->getApplicationReleasePath($this->application)}/vendor/bin/typo3cms'"
         );
         $this->assertCommandExecuted("php 'vendor/bin/typo3cms' 'extension:setup' 'foo,bar'");
+    }
+
+    protected function getMockShell()
+    {
+        $commands = &$this->commands;
+        $mockShell = $this->getMockBuilder(\TYPO3\Surf\Domain\Service\ShellCommandService::class)->getMock();
+        $mockShell
+            ->expects($this->any())
+            ->method('execute')
+            ->will($this->returnCallback(function ($command) use (&$commands, &$responses) {
+                if (is_array($command)) {
+                    $commands['executed'] = array_merge($commands['executed'], $command);
+                } else {
+                    $commands['executed'][] = $command;
+                    if (isset($responses[$command])) {
+                        return $responses[$command];
+                    }
+                }
+                return '';
+            }));
+        $mockShell
+            ->expects(self::any())
+            ->method('executeOrSimulate')
+            ->will($this->returnCallback(function ($command) use (&$commands, &$responses) {
+                if (is_array($command)) {
+                    $commands['executed'] = array_merge($commands['executed'], $command);
+                } else {
+                    $commands['executed'][] = $command;
+                    if (isset($responses[$command])) {
+                        return $responses[$command];
+                    }
+                }
+                return '';
+            }));
+        return $mockShell;
+    }
+
+    protected function getMockTaskForConsoleVersion(string $consoleVersion)
+    {
+        $mockTask = $this->getMockBuilder(\TYPO3\Surf\Task\TYPO3\CMS\SetUpExtensionsTask::class)
+            ->setMethods(['getConsoleVersion'])->getMock();
+        $mockTask->expects($this->once())->method('getConsoleVersion')->willReturn($consoleVersion);
+        $mockTask->setShellCommandService($this->getMockShell());
+        return $mockTask;
     }
 }
