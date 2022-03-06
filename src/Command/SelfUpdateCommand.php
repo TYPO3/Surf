18ad60cb615d1e27312abb29b4193bfdf7a28412
Sebastian Schreiber <me@schreibersebastian.de>
2022-03-06 22:01:38 +0100

    Update SelfUpdateCommand.php

--- src/Command/SelfUpdateCommand.php
+++ src/Command/SelfUpdateCommand.php
@@ -4,15 +4,15 @@ declare(strict_types=1);
 
 namespace TYPO3\Surf\Command;
 
-use Humbug\SelfUpdate\Strategy\GithubStrategy;
-use Humbug\SelfUpdate\Updater;
-use Phar;
 /*
  * This file is part of TYPO3 Surf.
  *
  * For the full copyright and license information, please view the LICENSE.txt
  * file that was distributed with this source code.
  */
+use Humbug\SelfUpdate\Strategy\GithubStrategy;
+use Humbug\SelfUpdate\Updater;
+use Phar;
 use Symfony\Component\Console\Application;
 use Symfony\Component\Console\Command\Command;
 use Symfony\Component\Console\Input\InputInterface;
