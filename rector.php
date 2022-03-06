cc9547f3e5de36b0fffc00c9b5fdcf01bef6e92f
sabbelasichon <sabbelasichon@users.noreply.github.com>
2022-03-06 19:43:22 +0000

    Apply php-cs-fixer changes

--- rector.php
+++ rector.php
@@ -6,7 +6,6 @@ use Rector\Core\Configuration\Option;
 use Rector\Php74\Rector\LNumber\AddLiteralSeparatorToNumberRector;
 use Rector\PHPUnit\Set\PHPUnitSetList;
 use Rector\Set\ValueObject\LevelSetList;
-use Rector\Set\ValueObject\SetList;
 use Symfony\Component\DependencyInjection\Loader\Configurator\ContainerConfigurator;
 
 return static function (ContainerConfigurator $containerConfigurator): void {
