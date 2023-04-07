2e55681f4c4c3c3a62187df6f07aa66eb3c7417d
Sebastian Schreiber <me@schreibersebastian.de>
2023-04-07 15:46:14 +0200

    TASK: Move away from symfony/http-kernel

--- src/Cli/Symfony/ConsoleKernel.php
+++ src/Cli/Symfony/ConsoleKernel.php
@@ -14,38 +14,37 @@ namespace TYPO3\Surf\Cli\Symfony;
 use Phar;
 use Psr\Log\LoggerAwareInterface;
 use Psr\Log\LoggerInterface;
+use Symfony\Component\Config\FileLocator;
 use Symfony\Component\Config\Loader\LoaderInterface;
+use Symfony\Component\DependencyInjection\Container;
 use Symfony\Component\DependencyInjection\ContainerAwareInterface;
 use Symfony\Component\DependencyInjection\ContainerBuilder;
 use Symfony\Component\DependencyInjection\ContainerInterface;
+use Symfony\Component\DependencyInjection\Dumper\PhpDumper;
+use Symfony\Component\DependencyInjection\Loader\PhpFileLoader;
 use Symfony\Component\DependencyInjection\Reference;
-use Symfony\Component\HttpKernel\Kernel;
 use TYPO3\Surf\Cli\Symfony\CompilerPasses\CommandsToApplicationCompilerPass;
 use TYPO3\Surf\Domain\Service\ShellCommandService;
 use TYPO3\Surf\Domain\Service\ShellCommandServiceAwareInterface;
 
-final class ConsoleKernel extends Kernel
+final class ConsoleKernel
 {
-    /**
-     * @inheritDoc
-     */
-    public function registerBundles(): array
+    private bool $booted = false;
+    private ?Container $container = null;
+    private string $environment;
+    private ?string $projectDir = null;
+
+    public function __construct(string $environment = 'dev')
     {
-        return [];
+        $this->environment = $environment;
     }
 
-    /**
-     * @inheritDoc
-     */
-    public function registerContainerConfiguration(LoaderInterface $loader): void
+    private function registerContainerConfiguration(LoaderInterface $loader): void
     {
         $loader->load(__DIR__ . '/../../../Resources/services.php');
     }
 
-    /**
-     * @inheritDoc
-     */
-    protected function build(ContainerBuilder $container): void
+    private function build(ContainerBuilder $container): void
     {
         $container->addCompilerPass(new CommandsToApplicationCompilerPass());
         $container->registerForAutoconfiguration(
@@ -68,21 +67,92 @@ final class ConsoleKernel extends Kernel
         );
     }
 
-    public function getCacheDir(): string
+    public function boot(): void
+    {
+        if (true === $this->booted) {
+            return;
+        }
+        $this->initializeContainer();
+        $this->booted = true;
+    }
+
+    public function getContainer(): Container
+    {
+        if ($this->container === null) {
+            throw new \UnexpectedValueException('Boot the kernel before');
+        }
+
+        return $this->container;
+    }
+
+    private function initializeContainer(): void
+    {
+        if ($this->container !== null) {
+            return;
+        }
+
+        foreach (['cache' => $this->getCacheDir()] as $name => $dir) {
+            if (!is_dir($dir)) {
+                if (false === @mkdir($dir, 0777, true) && !is_dir($dir)) {
+                    throw new \RuntimeException(sprintf('Unable to create the "%s" directory (%s).', $name, $dir));
+                }
+            } elseif (!is_writable($dir)) {
+                throw new \RuntimeException(sprintf('Unable to write in the "%s" directory (%s).', $name, $dir));
+            }
+        }
+
+        $file = $this->getCacheDir() .'/container.php';
+
+        if (file_exists($file)) {
+            require_once $file;
+            /** @var Container $container */
+            $container = new \ProjectServiceContainer();
+        } else {
+            $container = new ContainerBuilder();
+            $loader = new PhpFileLoader($container, new FileLocator());
+
+            $this->registerContainerConfiguration($loader);
+            $this->build($container);
+
+            $container->compile();
+
+            $dumper = new PhpDumper($container);
+            file_put_contents($file, $dumper->dump());
+        }
+
+        $this->container = $container;
+    }
+
+    protected function getCacheDir(): string
     {
         if (Phar::running() !== '') {
             return sys_get_temp_dir() . '/_surf';
         }
 
-        return parent::getCacheDir();
+        return $this->getProjectDir().'/var/cache/'.$this->environment;
     }
 
-    public function getLogDir(): string
+    private function getProjectDir(): string
     {
-        if (Phar::running() !== '') {
-            return sys_get_temp_dir() . '/_surf_log';
+        if (null === $this->projectDir) {
+            $r = new \ReflectionObject($this);
+
+            $dir = $r->getFileName();
+
+            if (!$dir || !is_file($dir)) {
+                throw new \LogicException(sprintf('Cannot auto-detect project dir for kernel of class "%s".', $r->name));
+            }
+
+            $dir = $rootDir = \dirname($dir);
+            while (!is_file($dir.'/composer.json')) {
+                if ($dir === \dirname($dir)) {
+                    return $this->projectDir = $rootDir;
+                }
+                $dir = \dirname($dir);
+            }
+            $this->projectDir = $dir;
         }
 
-        return parent::getLogDir();
+        return $this->projectDir;
     }
 }
