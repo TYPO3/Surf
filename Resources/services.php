1d75331b61dc0ea3d581e185ba57aed19fb2977e
Simon Schaufelberger <simonschaufi@users.noreply.github.com>
2023-10-28 00:16:20 +0200

    [RELEASE] Release Surf 3.4.6

--- Resources/services.php
+++ Resources/services.php
@@ -35,7 +35,7 @@ return static function (ContainerConfigurator $containerConfigurator): void {
 
     $parameters->set('name', 'TYPO3 Surf');
 
-    $parameters->set('version', '3.4.5');
+    $parameters->set('version', '3.4.6');
 
     $services = $containerConfigurator->services();
 
