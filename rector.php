fa3ba3380c9a463faf89a1c93c9b94cf1db7f059
Simon Schaufelberger <simonschaufi@users.noreply.github.com>
2025-05-10 19:37:47 +0200

    [TASK] Update Rector and phpstan

--- rector.php
+++ rector.php
@@ -10,25 +10,26 @@ declare(strict_types=1);
  */
 
 use Rector\Config\RectorConfig;
-use Rector\Php74\Rector\LNumber\AddLiteralSeparatorToNumberRector;
+use Rector\PHPUnit\CodeQuality\Rector\Class_\PreferPHPUnitThisCallRector;
+use Rector\PHPUnit\CodeQuality\Rector\ClassMethod\ReplaceTestAnnotationWithPrefixedFunctionRector;
 use Rector\PHPUnit\Set\PHPUnitSetList;
 use Rector\Set\ValueObject\LevelSetList;
 
 return static function (RectorConfig $rectorConfig): void {
     $rectorConfig->importNames();
-
-    // get parameters
+    $rectorConfig->importShortClasses(false);
     $rectorConfig->paths([
         __DIR__ . '/src',
         __DIR__ . '/tests',
     ]);
     $rectorConfig->skip([
-        AddLiteralSeparatorToNumberRector::class,
+        // PHPUnit
+        PreferPHPUnitThisCallRector::class,
+        ReplaceTestAnnotationWithPrefixedFunctionRector::class,
     ]);
     $rectorConfig->sets([
         PHPUnitSetList::PHPUNIT_CODE_QUALITY,
-        PHPUnitSetList::PHPUNIT_SPECIFIC_METHOD,
-        PHPUnitSetList::PHPUNIT_EXCEPTION,
+        PHPUnitSetList::PHPUNIT_90,
         LevelSetList::UP_TO_PHP_74,
     ]);
 };
